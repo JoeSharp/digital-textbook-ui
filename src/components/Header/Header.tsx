2764ccaf2bd8cac920c4e712b4426dc2a7b3886c
GCHQ 11 <gchq.developer.11@gmail.com>
2020-03-20 05:11:20 +0000

    removing their stuff, adding first component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { FunctionComponent } from "react";
+
+const Header: FunctionComponent = () => {
+  return (
+    <div>
+      <h1>Teach Code</h1>
+    </div>
+  );
+};
+
+export default Header;
