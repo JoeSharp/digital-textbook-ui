e0805d3b93a07122f44b552e0a503a7abf6b196c
GCHQ 11 <gchq.developer.11@gmail.com>
2020-03-27 15:24:20 +0000

    added mocking of REST API in Storybook

--- src/components/CoursesList/CoursesList.stories.tsx
+++ src/components/CoursesList/CoursesList.stories.tsx
@@ -1,9 +1,23 @@
 import * as React from "react";
 import CoursesList from "./CoursesList";
+import fetchMock from "fetch-mock";
+import { CourseType } from "../../types";
 
 export default {
   title: "Courses List",
   component: CoursesList
 };
 
-export const Basic = () => <CoursesList />;
+export const Basic = () => {
+  const payload: CourseType[] = [
+    {
+      _id: "1",
+      name: "Test Course 1"
+    }
+  ];
+  fetchMock
+    .restore()
+    .getOnce(`${process.env.REACT_APP_SERVICE_BASE_URL}/courses`, payload);
+
+  return <CoursesList />;
+};
