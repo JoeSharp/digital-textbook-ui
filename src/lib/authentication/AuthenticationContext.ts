c1e7b078a9f63a96895790746b0290573ea0e204
Joe Sharp <ratracejoe@hotmail.com>
2020-04-03 19:24:52 +0100

    fixing react import

--- src/lib/authentication/AuthenticationContext.ts
+++ src/lib/authentication/AuthenticationContext.ts
@@ -1,4 +1,4 @@
-import * as React from "./node_modules/react";
+import * as React from "react";
 
 interface AuthenticationContextApi {
   idToken?: string;
@@ -14,8 +14,8 @@ const defaultContext: AuthenticationContextApi = {
   },
 };
 
-const AuthenticationContext: React.Context<
-  AuthenticationContextApi
-> = React.createContext(defaultContext);
+const AuthenticationContext: React.Context<AuthenticationContextApi> = React.createContext(
+  defaultContext
+);
 
 export default AuthenticationContext;
